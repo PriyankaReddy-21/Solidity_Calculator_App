19383171e436943a3b3c73b9ab7a2fda9cc61caf
PriyankaReddy-21 <125461903+PriyankaReddy-21@users.noreply.github.com>
2023-03-11 21:37:46 +0530

    Add files via upload

---
+++ my_calc_app.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Calc_App {
+
+
+    function Add(uint8 num_1, uint8 num_2) public pure returns (uint256) {
+        return (num_1 + num_2);
+    }
+
+    function Sub(uint8 num_1, uint8 num_2) public pure returns (uint256){
+        require(num_1 > num_2, "First number should be higher than second");
+        return (num_1 - num_2);
+    }
+
+    function Mul(uint8 num_1, uint8 num_2) public pure returns (uint256) {
+        return( num_1 * num_2 );
+    }
+
+    function Div(uint8 num_1, uint8 num_2) public pure returns (uint256){
+        require(num_2 != 0, "Cannot divide by 0");
+        return (num_1 / num_2);
+    }
+
+    function Mod(uint8 num_1, uint8 num_2) public pure returns (uint256){
+        require(num_2 != 0, "Cannot divide by 0");
+        return (num_1 % num_2);
+    }
+
+    function square (uint8 num) public pure returns (uint256){
+        return num*num;
+    }
+
+    function cube (uint8 num) public pure returns (uint256){
+        return num*num*num;
+    }
+
+    function exp (uint8 num_1, uint8 num_2) public pure returns (uint256){
+        return num_1**num_2;
+    }
+
+}
